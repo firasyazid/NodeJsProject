614684ca894ee39db3082a8fed64a7830d4604c3
firasyazid <98762038+firasyazid@users.noreply.github.com>
2022-06-20 02:06:55 +0100

    push

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.options('*',cors ());
 app.use(express.json()); 
 app.use(morgan('tiny'));
 
-//Routers
+//Routes
 app.use(`${api}/products`, productsRouter);
 app.use(`${api}/categories`, categoriesRouter);
 app.use(`${api}/orders`, ordersRouter);
